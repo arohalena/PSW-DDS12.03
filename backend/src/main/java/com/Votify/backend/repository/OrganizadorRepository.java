94ec0662aedecb75b9aef631e307a6704f62e61a
Zhijian Wu Wang <zwsocracked@gmail.com>
2026-03-02 01:15:24 +0100

    Arreglado clase modelo Organizador faltaba por poner la columna del id

---
+++ backend/src/main/java/com/Votify/backend/repository/OrganizadorRepository.java
@@ -0,0 +1,13 @@
+package com.Votify.backend.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Votify.backend.model.Organizador;
+
+public interface OrganizadorRepository extends JpaRepository<Organizador, UUID>{
+    
+    boolean existsByEmail(String email);
+
+}
