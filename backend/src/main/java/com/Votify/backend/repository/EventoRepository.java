2fede4144c52ea4f7afc8a3e2d93f45c492a2824
Zhijian Wu Wang <zwsocracked@gmail.com>
2026-03-02 21:32:54 +0100

    Completado el repositorio de evento

---
+++ backend/src/main/java/com/Votify/backend/repository/EventoRepository.java
@@ -0,0 +1,12 @@
+package com.Votify.backend.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Votify.backend.model.Evento;
+
+public interface EventoRepository extends JpaRepository<Evento, UUID>{
+    
+    boolean existsByCodigoAccesoPublico(String codigo);
+}
