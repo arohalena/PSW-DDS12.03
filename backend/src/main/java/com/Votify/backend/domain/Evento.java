8a134e943950eff2e42dc4363abb6495df4d8577
Zhijian Wu Wang <zwsocracked@gmail.com>
2026-03-11 10:32:49 +0100

    Evento domain hecho falta el resto

--- backend/src/main/java/com/Votify/backend/domain/Evento.java
+++ backend/src/main/java/com/Votify/backend/domain/Evento.java
@@ -1,18 +1,23 @@
 package com.Votify.backend.domain;
 
 import com.Votify.backend.model.TipoEvento;
+import java.time.OffsetDateTime;
 
 public abstract class Evento {
     
     protected String nombre;
     protected String codigoAccesoPublico;
     protected String descripcion;
+    protected OffsetDateTime fecha_fin;
+    protected OffsetDateTime fecha_inicio;
 
-    public Evento(String nombre, String descripcion, String codigoAccesoPublico){
+    public Evento(String nombre, String descripcion, String codigoAccesoPublico, OffsetDateTime fecha_fin, OffsetDateTime fecha_inicio){
 
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.codigoAccesoPublico = codigoAccesoPublico;
+        this.fecha_fin = fecha_fin;
+        this.fecha_inicio = fecha_inicio;
 
     }
 
@@ -21,5 +26,7 @@ public abstract class Evento {
     public String getNombre() {return nombre;}
     public String getCodigoAccesoPublico() {return codigoAccesoPublico;}
     public String getDescripcion() {return descripcion;}
+    public OffsetDateTime getFechaFin() {return fecha_fin;}
+    public OffsetDateTime getFechaInicio() {return fecha_inicio;}
 
 }
