465434e05a4a90afa7ad1a4a5b71880dccbc7804
Sergio Ibáñez Romero <SIBAROM@upv.edu.es>
2026-04-01 13:34:25 +0200

    Cambio en CompetidorMO.equipos de @OneToOne a @OneToMany pq daba error

--- backend/src/main/java/com/Votify/backend/model/CompetidorMO.java
+++ backend/src/main/java/com/Votify/backend/model/CompetidorMO.java
@@ -3,9 +3,9 @@ package com.Votify.backend.model;
 import java.util.List;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -25,7 +25,7 @@ public class CompetidorMO extends ModeloBaseMO {
     @Column(nullable = false)
     private String password;
 
-	@OneToOne
+	@OneToMany
     @JoinColumn(name = "equipo_id", nullable = false)
     private List<EquipoMO> equipos;
 
