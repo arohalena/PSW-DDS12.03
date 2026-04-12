4d6d8905f1ae0589463500df069cdadaf007f34a
arohalena <szheng@upv.edu.es>
2026-04-12 22:42:31 +0200

    Back para cargar los proyectos existentes por evento

--- backend/src/main/java/com/Votify/backend/controller/ProyectoController.java
+++ backend/src/main/java/com/Votify/backend/controller/ProyectoController.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Votify.backend.dto.CargaProyectosRestResponse;
 import com.Votify.backend.model.ProyectoMO;
+import com.Votify.backend.service.CargaProyectosService;
 import com.Votify.backend.service.GenericService;
 import com.Votify.backend.service.ProyectoService;
 
 import lombok.RequiredArgsConstructor;
+
 
 
 
@@ -24,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class ProyectoController extends GenericController<ProyectoMO>{
 
     private final ProyectoService proyectoService;
+    private final CargaProyectosService cargaProyectosService;
 
     @Override
     protected GenericService<ProyectoMO> getService(){
@@ -43,5 +47,17 @@ public class ProyectoController extends GenericController<ProyectoMO>{
     public ProyectoMO crear(@RequestBody ProyectoMO proyecto){
 
         return proyectoService.crear(proyecto);
-    }    
+    }
+
+    @GetMapping("/evento/{eventoId}/cargaProyectos")
+    public CargaProyectosRestResponse cargaProyectos(@PathVariable UUID eventoId) {
+        try {
+			CargaProyectosRestResponse response = cargaProyectosService.cargaProyectos(eventoId);
+			System.out.println("Respuesta de proyectos: " + response);
+			return response;
+		} catch (Exception e) {
+			return new CargaProyectosRestResponse("Error cargando los proyectos: " + e.getMessage(), null);
+		}
+    }
+    
 }
