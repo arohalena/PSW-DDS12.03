3e06f119967ddd20dc551a5fbea17fad72e9eed4
Zhijian Wu Wang <zwsocracked@gmail.com>
2026-04-13 19:35:29 +0200

    Añadido ambos repositorios

--- backend/src/main/java/com/Votify/backend/repository/CriterioEvaluacionRepository.java
+++ backend/src/main/java/com/Votify/backend/repository/CriterioEvaluacionRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Votify.backend.model.CriterioEvaluacionMO;
 
-public interface CriterioEvaluacionRepository extends JpaRepository<CriterioEvaluacionMO, UUID> {
-    List<CriterioEvaluacionMO> findByEvento_IdOrderByOrdenAsc(UUID eventoId);
+public interface CriterioEvaluacionRepository extends JpaRepository<CriterioEvaluacionMO, UUID>{
+    
+    List<CriterioEvaluacionMO> findByEvento_IdOrderByAsc(UUID eventoId);
+    
 }
