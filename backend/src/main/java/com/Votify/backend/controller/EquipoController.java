098637b23b3c723667be0161004b472a94d53424
arohalena <szheng@upv.edu.es>
2026-04-13 22:04:13 +0200

    getEquiposByEventoId

--- backend/src/main/java/com/Votify/backend/controller/EquipoController.java
+++ backend/src/main/java/com/Votify/backend/controller/EquipoController.java
@@ -1,9 +1,14 @@
 package com.Votify.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.UUID;
 
 import com.Votify.backend.model.EquipoMO;
 import com.Votify.backend.service.EquipoService;
@@ -26,5 +31,10 @@ public class EquipoController extends GenericController<EquipoMO> {
     @PostMapping("/crear")
     public EquipoMO crear(@RequestBody EquipoMO equipo) {
         return equipoService.crear(equipo);
+    }
+
+    @GetMapping("/evento/{eventoId}")
+    public List<EquipoMO> getEquiposByEvento(@PathVariable UUID eventoId) {
+        return equipoService.getEquiposPorEvento(eventoId);
     }
 }
