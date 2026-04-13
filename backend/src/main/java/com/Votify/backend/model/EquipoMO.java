6d64d7b30334049e65877c4b4de4d206157d8f10
Zhijian Wu Wang <zwsocracked@gmail.com>
2026-04-13 22:02:09 +0200

    Arreglado un fallo que tenia con los equipos

--- backend/src/main/java/com/Votify/backend/model/EquipoMO.java
+++ backend/src/main/java/com/Votify/backend/model/EquipoMO.java
@@ -1,5 +1,9 @@
 package com.Votify.backend.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -9,7 +13,6 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -30,7 +33,8 @@ public class EquipoMO extends ModeloBaseMO {
     @ManyToOne
     @JoinColumn(name = "evento_id", nullable = false)
     private EventoMO evento;
-
+    
+    @JsonIgnore
     @OneToMany(mappedBy = "equipo")
     @JsonIgnore
     private List<CompetidorEventoMO> competidores;
