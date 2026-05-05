a2c026ea6cb443a1aa41f004291f994d09be3f93
arohalena <szheng@upv.edu.es>
2026-05-05 13:28:10 +0200

    fix:criterio zero

--- frontend/src/screens/event/CreateVotingModal.jsx
+++ frontend/src/screens/event/CreateVotingModal.jsx
@@ -191,9 +191,9 @@ function CreateVotingModal({ eventoId, eventoNombre, onClose, onCreated }) {
           return `La suma de pesos de los criterios deben sumar 100%. Ahora suman ${totalPeso}%.`;
         }
 
-        /*if (zeroWeight) {
+        if (zeroWeight.length > 0) {
           return "No puede haber un criterio con peso 0%";
-        }*/
+        }
 
       }
     }
