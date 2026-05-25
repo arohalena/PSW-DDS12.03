4aca2ae0df7cf72ed46f0ba754902210b2a75e40
arohalena <szheng@upv.edu.es>
2026-05-25 19:28:40 +0200

    odio las cors

--- backend/src/main/java/com/Votify/backend/controller/AuthController.java
+++ backend/src/main/java/com/Votify/backend/controller/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController {
 
     private final AuthService authService;
